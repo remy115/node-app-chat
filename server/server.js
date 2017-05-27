e9f441e7a2db4bf9774488b2147fc88a70ea911d
Remy Ponso <remy@central>
2017-05-27 13:52:24 -0300

    Greet new user and warn others

--- server/server.js
+++ server/server.js
@@ -12,10 +12,16 @@ var io=socketIO(server);
 io.on('connect',(socket)=>{
     console.log('New user connected!');
 
+
     socket.emit('newMessage',{
-        from:'user01@email.com',
-        text:'Hi there! created on the server!',
-        createdAt:213232482
+        from:'Admin',
+        text:'Welcome to the chat app!',
+        createdAt:new Date().getTime()
+    });
+    socket.broadcast.emit('newMessage',{
+        from:'Admin',
+        text:'New user joined',
+        createdAt:new Date().getTime()
     });
 
     socket.on('disconnect',()=>{
